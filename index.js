5969e2e329798f761428148b4173934b3bc49a67
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-04-07 10:47:12 -0700

    try out pg promis npm

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const db = require('./queries')
+
+//pg promise module, pool is doing this already no need to use
+// var pgp = require('pg-promise')(/*options*/)
+// var db = pgp('postgres://username:password@host:port/database')
+
+
+
+// db.one('SELECT $1 AS value', 123)
+//   .then(function (data) {
+//     console.log('DATA:', data.value)
+//   })
+//   .catch(function (error) {
+//     console.log('ERROR:', error)
+//   })
+
+
+
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//test route
-app.get('/', (request, response) => { 
-response.json({ info: "Test Message Testing" })
-})
+
 
 /* ===========================  POOL  =================================
 ======================================================================= */
@@ -28,17 +41,27 @@ const pool = new Pool({
 })
 /* =====================  ROUTES / QUERIES  ===========================
 ======================================================================= */
+//test route
+app.get('/', (request, response) => { 
+    response.json({ info: "Test Message Testing" })
+    })
 
-app.get('/contacts', async (req, res) =>{
-    const client = await pool.connect();
-    const contactsTable = await client.query('SELECT * FROM events WHERE id = $1', [req.params.id]); 
-    res.json(contactsTable.rows[0]);
-    client.release();
-})
-// app.get('/contacts/:id', db.getContactsById)
+app.get('/contacts',async (req, res) =>{
+    const client = await pool.connect(); //make request with client inside the pool
+    const contactsTable = await client.query('SELECT * FROM contractors_report.contacts LIMIT 10'); //query that runs to pull pg data
+    res.json(contactsTable.rows); //the queried data as response in json form
+    console.log('hello') //testing for connection
+    client.release();//release client back to pool
+    })
+// app.get('/contacts/:id',  async (req, res) =>{
+//     const client = await pool.connect();
+//     const contactsTable = await client.query('SELECT * FROM contractors_report.contacts WHERE id = $1', [req.params.id]); 
+//     res.json(contactsTable.rows[0]);
+//     client.release();
+// })
 // app.get('/contacts', async (req, res) =>{
 //     const client = await pool.connect();
-//     const contactsTable = await client.query('SELECT * FROM contacts');
+//     const contactsTable = await client.query('SELECT * FROM contractors_report.contacts LIMIT 10');
 //     res.json(contactsTable.rows);
 
 //     client.release();
